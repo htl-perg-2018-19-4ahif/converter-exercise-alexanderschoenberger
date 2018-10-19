5f52bb1671e79c78499339cd3d35fa068dae64db
Alexander Schoenberger <alexanderschoenberger@gmx.at>
2018-10-19 18:20:55 +0200

    HUE Daniel Hofer Alexander Schönberger

--- convert.js
+++ convert.js
@@ -1,1 +1,9 @@
-console.log('Not implemented yet!');
+const convert = require('convert-units')
+try {
+    let args = process.argv.slice(2);
+    let number = convert(args[0]).from(args[1]).to(args[3]);
+    if (args[2] != 'to' || isNaN(number)) throw err;
+    else console.log(args[0] + ' ' + args[1] + ' are ' + number + ' ' + args[3]);
+} catch (err) {
+    console.log('Invalid parameters');
+}
